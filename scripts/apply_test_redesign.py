76a2e69e0c4f33ab04013816cc79825a0e55acf7
84lorinw-a11y <310772583+84lorinw-a11y@users.noreply.github.com>
2026-09-21 14:23:56 -0500

    Refresh redesigned style cache

--- scripts/apply_test_redesign.py
+++ scripts/apply_test_redesign.py
@@ -395,7 +395,7 @@ def replace_legacy_header(document: str, header: str) -> str:
 
 
 def inject_assets(document: str) -> str:
-    css = f'{TEST_BASE}assets/kc-redesign-v1.css?v=4'
+    css = f'{TEST_BASE}assets/kc-redesign-v1.css?v=5'
     js = f'{TEST_BASE}assets/kc-redesign-v1.js?v=2'
     if css not in document:
         document = document.replace(
